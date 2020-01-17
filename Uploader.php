de1a0324a1c1e8c6ca049d49499e88e79807d57a
JRMOLINA2020 <JRMOLINA2020@GMAIL.COM>
2020-01-17 15:04:08 -0500

    unitario

--- Uploader.php
+++ Uploader.php
@@ -1,9 +1,9 @@
 <?php
-$url="https://fepruebas.amovil.co:8080/api/";
-$ruta=$url."integration/upload/";
+$url = "https://fepruebas.amovil.co:8080/api/";
+$ruta = $url . "integration/upload/";
 
 
-$file = ''.$_SERVER['DOCUMENT_ROOT'].'/delta/Ejemplo JSON.zip';
+$file = '' . $_SERVER['DOCUMENT_ROOT'] . '/delta/Ejemplo JSON.zip';
 $mime = mime_content_type($file);
 $info = pathinfo($file);
 $name = $info['basename'];
@@ -15,20 +15,21 @@ $data = array(
 
 $ch = curl_init();
 curl_setopt(
-    $ch, CURLOPT_HTTPHEADER, array(
-    'Authorization: Token 8192f7fa349f0aad02bd9b55e5ff41ebe8bdc6d9',
-    'Content-Type: multipart/form-data',
+    $ch,
+    CURLOPT_HTTPHEADER,
+    array(
+        'Authorization: Token 018e25f173773fc89f455c99e92af664a10f76f5',
+        'Content-Type: multipart/form-data',
     )
-    );
+);
 
 
 curl_setopt($ch, CURLOPT_URL, $ruta);
-curl_setopt($ch, CURLOPT_POST,1);
-curl_setopt ($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
 curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-curl_setopt ($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $result = curl_exec($ch);
 
-curl_close ($ch);
+curl_close($ch);
 echo $result;
-?>
--- auth.php
+++ auth.php
@@ -1,45 +1,74 @@
 <?php
 
 
- class Login{
+class Login
+{
     public $username;
     public $password;
     public $url;
+    public $urlU;
+    public $ruta;
     public $token;
 
-     // Constructor
-     public function __construct(){
+    // Constructor
+    public function __construct()
+    {
         $this->username = 'delta@amovil.co';
         $this->password = 'Delta2019$';
         $this->url = 'https://fepruebas.amovil.co:8080/api/auth/token/';
-
+        $this->urlU = 'https://fepruebas.amovil.co:8080/api/';
+        $this->ruta = $this->urlU . "integration/upload/";
     }
 
-
-    public function auth(){
+    public function auth()
+    {
         $ch = curl_init();
         $headers = array(
             'Accept: application/json',
             'Content-Type: application/json',
-        
-            );
-        
-        curl_setopt ($ch, CURLOPT_URL, $this->url);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, "username=".$this->username."&password=".$this->password.";");
-        curl_setopt ($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-        curl_setopt ($ch, CURLOPT_POST, 1);
-        curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
+
+        );
+
+        curl_setopt($ch, CURLOPT_URL, $this->url);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, "username=" . $this->username . "&password=" . $this->password . ";");
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         $result = curl_exec($ch);
         //obteniendo el token
-        $result = json_decode($result, true); 
+        $result = json_decode($result, true);
         $token = $result['token'];
         //pasando el token
         $this->token = $token;
-        $this->store();
-       
-        }
-        public function store(){
-         echo $this->token;
-        }
+        $this->Uploader();
+    }
+    public function Uploader()
+    {
+        $token = "Token" . " " . $this->token;
+        $file = '' . $_SERVER['DOCUMENT_ROOT'] . '/delta/Ejemplo JSON.zip';
+        $mime = mime_content_type($file);
+        $info = pathinfo($file);
+        $name = $info['basename'];
+        $output = new CURLFile($file, $mime, $name);
+        $data = array(
+            "data" => $output,
+        );
+        $ch = curl_init();
+        curl_setopt(
+            $ch,
+            CURLOPT_HTTPHEADER,
+            array(
+                'Authorization:' . $token,
+                'Content-Type: multipart/form-data',
+            )
+        );
+        curl_setopt($ch, CURLOPT_URL, $this->ruta);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $result = curl_exec($ch);
+        curl_close($ch);
+        echo $result;
+    }
 }
-?>
